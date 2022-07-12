6e2c12c31f690e69542c79703af0db1c8135a851
Ratko Ćosić <dedalus123@gmail.com>
2022-07-12 13:37:32 +0200

    unique indexes

--- Server/Data/JournalDbContext.cs
+++ Server/Data/JournalDbContext.cs
@@ -12,20 +12,28 @@ namespace EDU_Journal.Server.Data
         public DbSet<Vacation> Vacations { get; set; } //table
         public DbSet<SickLeave> SickLeaves { get; set; } //table
 
-
         public JournalDbContext(DbContextOptions<JournalDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)//prilikom kreiranja baze podataka - što se sve kreira u njoj (relacije USER- WORKINg DAy)
         {
             modelBuilder.Entity<User>(entity =>
             {
-                entity.Property(e => e.Email); //lambda 
                 entity.HasIndex(i => i.Email).IsUnique();
             });
 
             modelBuilder.Entity<WorkingDay>(entity =>
             {
-                entity.HasIndex(i => new {i.UserId, i.Date}); //  Unique Key
+                entity.HasIndex(i => new {i.UserId, i.Date}).IsUnique(); //  Unique Key
+            });
+
+            modelBuilder.Entity<PublicHoliday>(entity =>
+            {
+                entity.HasIndex(i => i.Date).IsUnique();
+            });
+
+            modelBuilder.Entity<SickLeave>(entity =>
+            {
+                entity.HasIndex(i => i.Name).IsUnique();
             });
         }
     }
